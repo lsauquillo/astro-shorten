e0fb6f17c32e74fae16ee9f4a9d77682d92f6a86
Luis <lsauquillo@gmail.comgit condfig --global user.name lsauquillo>
2026-05-01 12:17:11 +0200

    Add Netlify adapter for Astro SSR

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
+import netlify from '@astrojs/netlify';
 
-// https://astro.build/config
 export default defineConfig({
   output: 'server',
+  adapter: netlify(),
   integrations: [vue()],
 });
